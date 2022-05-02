f6a57fd5c216d41ba3d8d79da0972aebcf56442c
Anna Benke <anna.benke@solarwinds.com>
2022-05-02 18:25:55 +0200

    tasks in groups

--- app/worker.py
+++ app/worker.py
@@ -2,8 +2,11 @@ from uuid import uuid4
 
 import dramatiq
 from PyPDF2 import PdfFileReader
+from dramatiq import group
 from pdf2image import convert_from_path
 from dramatiq.brokers.rabbitmq import RabbitmqBroker
+from dramatiq.results.backends import RedisBackend
+from dramatiq.results import Results
 
 from db import queries
 from config import Config
@@ -11,6 +14,10 @@ from utils.document import get_document_path
 from utils.page import get_page_path, resize_page_if_needed
 
 rabbitmq_broker = RabbitmqBroker(host="rabbitmq")
+dramatiq.set_broker(rabbitmq_broker)
+
+result_backend = RedisBackend(url='redis://redis')
+rabbitmq_broker.add_middleware(Results(backend=result_backend))
 dramatiq.set_broker(rabbitmq_broker)
 
 
@@ -29,35 +36,50 @@ def upload_document(file):
     return document_id
 
 
-@dramatiq.actor
+@dramatiq.actor(store_results=True)
 def upload_pages(document_id):
     document_path = get_document_path(document_id)
 
     reader = PdfFileReader(document_path)
     pages_count = reader.numPages
+
+    process_image_tasks = []
     for page_number in range(1, pages_count + 1):
 
         # Process every page separately.
-        process_image.send(document_id, page_number)
+        item = process_image.message(document_id, page_number)
+        process_image_tasks.append(item)
+
+    result = group(process_image_tasks).run(delay=500)
+
+    task_timeout_in_ms = 60 * 60 * 1000
+    result.wait(timeout=task_timeout_in_ms)
+
+    # Some sophisticated logger should replace prints and there should be more logs in general.
+    print('Done')
 
     # Update document status and number of pages.
     update_document_info.send(document_id, pages_count)
 
 
-@dramatiq.actor()
+@dramatiq.actor(store_results=True)
 def process_image(document_id, page_number):
+    print(f'Processing page {page_number}')
+
     document_path = get_document_path(document_id)
     image = convert_from_path(document_path, first_page=page_number, last_page=page_number + 1)
     page_path = get_page_path(document_id, page_number)
     image[0].save(page_path, Config.PAGE_EXTENSION.upper())
 
+    print(f'Resizing image {page_path}')
     # Make image fit into given pixels rectangle.
     resize_page_if_needed(page_path)
 
+    print(f'Storing page {page_number} into DB')
     # Create DB entry.
     queries.create_page(document_id, page_path, page_number)
 
 
-@dramatiq.actor
+@dramatiq.actor(store_results=True)
 def update_document_info(document_id, pages_count):
     queries.finalize_document(document_id=document_id, num_of_pages=pages_count)
