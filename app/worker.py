7d21063acdead0b9ddeda8f65af2e43d5a7370c0
Anna Benke <anna.benke@solarwinds.com>
2022-05-02 13:40:04 +0200

    iterative image processing

--- app/worker.py
+++ app/worker.py
@@ -1,8 +1,9 @@
 from uuid import uuid4
 
 import dramatiq
-from PIL import ImageOps
 from PIL import Image
+from PIL import ImageOps
+from PyPDF2 import PdfFileReader
 from pdf2image import convert_from_path
 from dramatiq.brokers.rabbitmq import RabbitmqBroker
 
@@ -32,19 +33,24 @@ def upload_document(file):
 
 @dramatiq.actor
 def upload_pages(document_id):
-
     document_path = get_document_path(document_id)
-    images = convert_from_path(document_path)
 
-    for page_number in range(len(images)):
-        filepath = get_page_path(document_id, page_number)
-        images[page_number].save(filepath, 'PNG')
-        resize_if_needed(filepath)
+    reader = PdfFileReader(document_path)
+    pages_count = reader.numPages
+    for page_number in range(pages_count):
 
-        queries.create_page(document_id, filepath, page_number)
+        process_image(document_id, document_path, page_number)
 
     # Update document status and number of pages.
-    queries.update_document(document_id=document_id, num_of_pages=len(images))
+    queries.update_document(document_id=document_id, num_of_pages=pages_count)
+
+
+def process_image(document_id, document_path, page_number):
+    image = convert_from_path(document_path, first_page=page_number, last_page=page_number + 1)
+    filepath = get_page_path(document_id, page_number)
+    image[0].save(filepath, 'PNG')
+    resize_if_needed(filepath)
+    queries.create_page(document_id, filepath, page_number)
 
 
 def resize_if_needed(filepath):
